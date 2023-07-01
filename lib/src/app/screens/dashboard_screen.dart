52e6d51199c527c7da1e11c92f5682d4b7c0c93f
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-07-01 12:17:58 +0530

    dashboard screen setup

--- lib/src/app/screens/dashboard_screen.dart
+++ lib/src/app/screens/dashboard_screen.dart
@@ -1,12 +1,42 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:tmdb_client/src/app/routes.gr.dart';
 
 @RoutePage()
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
-  }
+  Widget build(BuildContext context) => AutoTabsRouter(
+        routes: const [
+          HomeRoute(),
+          SearchRoute(),
+          ProfileRoute(),
+        ],
+        builder: (context, child) {
+          final tabsRouter = AutoTabsRouter.of(context);
+
+          return Scaffold(
+            body: child,
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: tabsRouter.activeIndex,
+              onTap: tabsRouter.setActiveIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  label: 'Home',
+                  icon: Icon(Icons.home),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Search',
+                  icon: Icon(Icons.search),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Profile',
+                  icon: Icon(Icons.person),
+                ),
+              ],
+            ),
+          );
+        },
+      );
 }
