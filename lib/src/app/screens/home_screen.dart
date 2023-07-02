b39df7b1d6c2d64b6643be0e9d226820800e11ef
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-07-02 17:47:40 +0530

    feat: popular movies

--- lib/src/app/screens/home_screen.dart
+++ lib/src/app/screens/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:tmdb_client/src/app/routes.gr.dart';
+import 'package:tmdb_client/src/features/popular_movies/widgets/popular_movies_widget.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const HomeScreen();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('TMDB Client')),
+        body: PopularMoviesWidget(
+          onItemPressed: (movie) => context.router.navigate(
+            const DetailsRoute(),
+          ),
+        ),
+      );
 }
+
